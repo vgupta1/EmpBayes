877937610e8bec637831e9cbfcacd2c316b5f4c2
Vishal Gupta <vishal.gupta@marshall.usc.edu>
2016-05-29 18:59:34 -0700

    adding the new tests

--- Normal Model/testHarness.jl
+++ Normal Model/testHarness.jl
@@ -23,19 +23,21 @@ type NormalBayesExp
 	thetas
 	noise
 	tau0
+	mu0
 end
 
-function NormalBayesExp(seed::Integer, tau0, n_max, frac_fit=.1, avg_tau=4.)
+function NormalBayesExp(seed::Integer, tau0, n_max, mu0, frac_fit=.1, avg_tau=4.)
 	srand(seed)
 	cs = rand(n_max) * 2./frac_fit   
 	taus = 2. * avg_tau * rand(n_max)
-	NormalBayesExp(cs, taus, zeros(Float64, n_max), zeros(Float64, n_max), tau0)
+	NormalBayesExp(cs, taus, zeros(Float64, n_max), zeros(Float64, n_max), tau0, mu0)
 end
 
 #cs taus remain fixed
 function sim!(o::NormalBayesExp, x, y, z)
 	randn!(o.thetas) 
 	o.thetas /= sqrt(o.tau0)
+	o.thetas += o.mu0
 	simData!(o, x, y, z)
 end
 
@@ -76,9 +78,8 @@ end
 function GammaExp(seed::Integer, alpha, beta, n_max, frac_fit = .1, avg_tau=2)
 	srand(seed)
 	cs = rand(n_max) * 2./frac_fit   
-	thetas = rand(Gamma(alpha, beta), n_max)
 	taus = 2. * avg_tau * rand(n_max)
-	GammaExp(cs, thetas, taus, zeros(Float64, n_max), alpha, beta)
+	GammaExp(cs, zeros(Float64, n_max), taus, zeros(Float64, n_max), alpha, beta)
 end
 
 function sim!(o::GammaExp, x, y, z)
@@ -87,11 +88,61 @@ function sim!(o::GammaExp, x, y, z)
 	simData!(o, x, y, z)	
 end
 
+#cs and taus fixed across all runs
+#thetas drawn as uniform [a, b]
+type UniformExp
+	cs
+	thetas
+	taus
+	noise
+	a
+	b
+end
+
+function UniformExp(seed::Integer, a, b, n_max, frac_fit = .1, avg_tau=2)
+	srand(seed)
+	cs = rand(n_max) * 2./frac_fit   
+	taus = 2. * avg_tau * rand(n_max)
+	UniformExp(cs, zeros(Float64, n_max), taus, zeros(Float64, n_max), a, b)
+end
+
+function sim!(o::UniformExp, x, y, z)
+	#cs & taus are fixed
+	rand!(Uniform(o.a, o.b), o.thetas) 	
+	simData!(o, x, y, z)	
+end
+
+
+#cs and taus fixed across all runs
+#thetas drawn as beta[a, b]
+type BetaExp
+	cs
+	thetas
+	taus
+	noise
+	a
+	b
+end
+
+function BetaExp(seed::Integer, a, b, n_max, frac_fit = .1, avg_tau=2)
+	srand(seed)
+	cs = rand(n_max) * 2./frac_fit   
+	taus = 2. * avg_tau * rand(n_max)
+	BetaExp(cs, zeros(Float64, n_max), taus, zeros(Float64, n_max), a, b)
+end
+
+function sim!(o::BetaExp, x, y, z)
+	#cs & taus are fixed
+	rand!(Beta(o.a, o.b), o.thetas) 	
+	simData!(o, x, y, z)	
+end
+
+#######################
 ### The usual bayesian normal setup
 ### costs and taus are random, but fixed over runs
-function test1_a(file_out, numRuns, n_grid, seed, tau0)
+function test_Gaussian(file_out, numRuns, n_grid, seed, tau0)
 	#build the sim object
-	o = NormalBayesExp(seed, tau0, maximum(n_grid))
+	o = NormalBayesExp(seed, tau0, maximum(n_grid), 0)
 
 	#run the testharness
 	#output files
@@ -101,8 +152,22 @@ function test1_a(file_out, numRuns, n_grid, seed, tau0)
 	close(f)
 end
 
+### Bayesian set-up with a non-zero mean
+### costs and taus are random, but fixed over runs
+function test_Gaussian(file_out, numRuns, n_grid, seed, tau0, mu0)
+	#build the sim object
+	o = NormalBayesExp(seed, tau0, maximum(n_grid), mu0)
+
+	#run the testharness
+	#output files
+	f = open("$(file_out)_$(mu0)_$(tau0)_$(seed).csv", "w")
+	writecsv(f, ["Run" "n" "Method" "YVal" "thetaVal" "time" "tau0"])
+	test_harness(f, numRuns, o, n_grid)
+	close(f)
+end
+
 ### The odd even set-up.  
-function test2_a(file_out, numRuns, n_grid, seed, odd_theta, odd_tau)
+function test_OddEven(file_out, numRuns, n_grid, seed, odd_theta, odd_tau)
 	#build the sim object
 	o = OddEvenExp(seed, maximum(n_grid), odd_theta, odd_tau)
 
@@ -115,7 +180,7 @@ function test2_a(file_out, numRuns, n_grid, seed, odd_theta, odd_tau)
 end
 
 ### The Gamma Test
-function test3_a(file_out, numRuns, n_grid, seed, alpha, beta)
+function test_Gamma(file_out, numRuns, n_grid, seed, alpha, beta)
 	o = GammaExp(seed, alpha, beta, maximum(n_grid))
 
 	f = open("$(file_out)_Gamma_$(alpha)_$(beta)_$(seed).csv", "w")
@@ -123,6 +188,27 @@ function test3_a(file_out, numRuns, n_grid, seed, alpha, beta)
 	test_harness(f, numRuns, o, n_grid)
 	close(f)
 end
+
+### The Uniform Test
+function test_Uniform(file_out, numRuns, n_grid, seed, a, b)
+	o = UniformExp(seed, a, b, maximum(n_grid))
+
+	f = open("$(file_out)_Uniform_$(a)_$(b)_$(seed).csv", "w")
+	writecsv(f, ["Run" "n" "Method" "YVal" "thetaVal" "time" "tau0"])
+	test_harness(f, numRuns, o, n_grid)
+	close(f)
+end
+
+### The Beta Test
+function test_Beta(file_out, numRuns, n_grid, seed, a, b)
+	o = BetaExp(seed, a, b, maximum(n_grid))
+
+	f = open("$(file_out)_Beta_$(a)_$(b)_$(seed).csv", "w")
+	writecsv(f, ["Run" "n" "Method" "YVal" "thetaVal" "time" "tau0"])
+	test_harness(f, numRuns, o, n_grid)
+	close(f)
+end
+
 
 ######################
 ####
@@ -258,6 +344,9 @@ end
 n_grid = [2^i for i = 8:12]
 
 #run some small examples to pre-compile for optimization
-test1_a("temp", 5, [100, 150], 87, 3)
-test2_a("temp2", 5,[100, 150], 87, 2, 2)
-test3_a("temp3", 5, [100, 150], 87, 1., 1.)
+test_Gaussian("temp_Gaussian", 5, [100, 150], 87, 3)
+test_OddEven("temp_OddEven", 5,[100, 150], 87, 2, 2)
+test_Gamma("temp_Gamma", 5, [100, 150], 87, 1., 1.)
+test_Uniform("temp_Uniform", 5, [100, 150], 87, 1, 2)
+test_Gaussian("temp_Gaussian2", 5, [100, 150], 87, 3, 5)
+test_Beta("temp_Uniform", 5, [100, 150], 87, .5, .5)
