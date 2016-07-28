58dc65a2d68a5cc82703db79ff6e5dc7933519b4
Vishal Gupta <vishal.gupta@marshall.usc.edu>
2016-07-28 16:03:01 -0700

    nothing imptnt

--- Normal Model/tests.jl
+++ Normal Model/tests.jl
@@ -1,7 +1,7 @@
 ## A small driver file to run the tests in parallel and combine them
-a = @spawn test_Gaussian("gaussian_exp", 10, n_grid, 8675309, 3, 0)
-b = @spawn test_Gaussian("gaussian_exp", 10, n_grid, 5165174290, 3, 0)
-c = @spawn test_Gaussian("gaussian_exp", 10, n_grid, 7462266, 3, 0)
+a = @spawn test_OddEven("odd_even_exp", 10, n_grid, 8675309, 2, 2)
+b = @spawn test_OddEven("odd_even_exp", 10, n_grid, 5165174290, 2, 2)
+c = @spawn test_OddEven("odd_even_exp", 10, n_grid, 7462266, 2, 2)
 
 println( fetch(a) )
 println( fetch(b) )
